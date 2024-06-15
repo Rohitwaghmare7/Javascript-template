4f3235929bbab315c53ac3a72677de3a782705d1
Rohit Waghmare <94979142+Rohitwaghmare7@users.noreply.github.com>
2024-06-15 08:37:21 +0000

    Updated About and Portfolio images

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/portfolio_landscape.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Portfolio landscape Background";
 
 /**
  * Project list
